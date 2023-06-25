65dcfd880e9c17251faf9dea180e2111fee23466
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2023-06-25 19:07:05 +0530

    Added Nginx as a reverse proxy

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function main() {
             .catch((err) => console.log("Error in connection: ", err))
 }
 
+app.enable('trust proxy');
 app.use(sessionMiddleware);
 app.use(express.json());
 
